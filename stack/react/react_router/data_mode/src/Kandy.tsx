43518dd7a16ce3fcaec2ce4f1bbe017e1f8a5ae5
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-17 15:54:45 +1300

    add Kandy.tsx for data_mode/

---
+++ stack/react/react_router/data_mode/src/Kandy.tsx
@@ -0,0 +1,9 @@
+function Kandy() {
+    return (
+        <div>
+        <h1>Kandy</h1>
+        </div>
+    );
+}
+
+export default Kandy;
--- stack/react/react_router/data_mode/src/main.tsx
+++ stack/react/react_router/data_mode/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.tsx'
+import Kandy from './Kandy.tsx';
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "/kandy",
+    Component: Kandy,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
